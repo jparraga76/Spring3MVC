6ddb63720bd67e77ab70695d7a7c43454f93d077
jparraga76 <57899140+jparraga76@users.noreply.github.com>
2019-11-19 09:09:44 +0100

    Update HelloController.java

--- src/main/java/com/mkyong/common/controller/HelloController.java
+++ src/main/java/com/mkyong/common/controller/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(final ModelMap model) {
   
-		model.addAttribute("message", "Spring Security Hello World");
+		model.addAttribute("message","Spring Security Hello World");
 		return "hello";
  
 	}
